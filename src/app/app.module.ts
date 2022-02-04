3d25db88d2ab4f64f7c8fc3d938054780e71b197
Murilo Abreu <murilo_abr@yahoo.com.br>
2022-02-04 15:08:37 -0300

    1.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { CorpoComponent } from './navegacao/corpo/corpo.component';
+import { RodapeComponent } from './navegacao/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CorpoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/navegacao/corpo/corpo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-corpo',
+  templateUrl: './corpo.component.html'
+})
+export class CorpoComponent {}
+
