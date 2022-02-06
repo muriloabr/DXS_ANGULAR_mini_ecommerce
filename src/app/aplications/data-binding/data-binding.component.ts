dda74339f0ec71c7f90af6fb339e0e0d1aaabdce
Murilo Abreu <murilo_abr@yahoo.com.br>
2022-02-06 14:15:04 -0300

    1.0.0

--- src/app/aplications/data-binding/data-binding.component.ts
+++ src/app/aplications/data-binding/data-binding.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { Component, OnInit, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-data-binding',
@@ -6,15 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 
-export class DataBindingComponent implements OnInit {
+export class DataBindingComponent implements OnInit, OnChanges, DoCheck {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  ngDoCheck(): void {
+    console.log('PASSOU PELO ngDoCheck!');
+    if(this.contadorCliques >= 10)
+    {       
+      this.textoBotao10Cliques = this.mensagensBotao10Cliques[1];
+    }
+  }
+ 
+  ngOnChanges(changes: SimpleChanges): void {
+      console.log('PASSOU PELO ngOnChanges!');      
+  }
+  
   private mensagensBotao10Cliques = [ '10 CLIQUES: LIBERAR', 'LIBERADO!' ];
   public contadorCliques: number = 0;
   public imgLink: string = 'https://dataxstudios.com.br/assets/images/DXS_software.png';
   public textoBotao10Cliques: string = this.mensagensBotao10Cliques[0];
+  public nome: string = "";
 
+  adicionarClique(): void {
+    this.contadorCliques++;
+  }
+  
+  zerarCliques(): void {
+    this.contadorCliques!=0?this.contadorCliques = 0:'';
+  }
+
+  apertouATecla(event: any){
+    this.nome = event.target.value;
+  }
 }
